8b8c9136ec3d3eef96b84dfe3430024f0343ca0d
Artem Vernyhora <artemvernyhora8@icloud.com>
2025-09-03 18:06:51 +0300

    fix header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
             <Link href="/">Home</Link>
           </li>
           <li>
-            <a href="/notes">Notes</a>
+            <Link href="/notes">Notes</Link>
           </li>
         </ul>
       </nav>
